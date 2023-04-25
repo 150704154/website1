b07ac2387002ba382fdc1962f67bd7f613c40415
150704154 <131166111+150704154@users.noreply.github.com>
2023-04-25 13:17:11 +0100

    Add files via upload

---
+++ Index.asm
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+ 
+
+<html> 
+
+ 
+
+<head>
+
+ 
+
+<title>The Ocean</title>
+
+</head>
+
+ 
+
+ 
+
+ 
+
+<body>
+
+ 
+
+<h1> The Ocean </h1>
+
+ 
+
+<p> There is only one global ocean but historically there are four named oceans, the Atlantic, Pacific, Indian and Arctic. However, most countries now recognise the Southern Antarctic as the fifth ocean </p>
+
+ 
+
+<a href=""> https://TheOcean.html</a>
+
+<img src=https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1652341068/EducationHub/photos/ocean-waves.jpg> <img src=https://thehill.com/wp-content/uploads/sites/2/2021/04/ca_robotsoceanhealth_042121istock.jpg>
+
+
+</body>
+
+ 
+
+</html>
